85a6307c7d9efc8e840044a4641f564fe8cf6a8c
Vinicius <viniciusc.moura@icloud.com>
2021-05-24 20:45:00 -0300

    Add prototype in library

--- libft.h
+++ libft.h
@@ -6,11 +6,30 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 18:56:56 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/05/24 19:44:45 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/05/24 20:43:11 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+char	*ft_strcpy(char *dest, const char *src);
+char	*strncpy(char *dest, const char *src, size_t n);
+char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
+char	*ft_strstr(const char *haystack, const char *needle);
+
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
