8506f534c65a4e48e3d97f0c452ef4c91d2ff62b
Vinicius <viniciusc.moura@icloud.com>
2021-05-24 20:14:24 -0300

    Up function strlcpy Basecamp

---
+++ ft_strlcpy.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/24 20:06:53 by vcavalca          #+#    #+#             */
+/*   Updated: 2021/05/24 20:13:27 by vcavalca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	count;
+	size_t	position;
+	size_t	hold;
+
+	hold = 0;
+	count = 1;
+	position = 0;
+	while (src[hold])
+	{
+		hold++;
+	}
+	if (hold != 0)
+	{
+		while (count < dstsize)
+		{
+			dst[position] = src[position];
+			count++;
+			position++;
+		}
+	}
+	dst[position] = '\0';
+	return (hold);
+}
