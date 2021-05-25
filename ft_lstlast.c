a94f13a9475ed23bbae3b8185f2766955075f262
Vinicius <viniciusc.moura@icloud.com>
2021-05-25 14:20:08 -0300

    Made lstlast

---
+++ ft_lstlast.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/25 14:18:10 by vcavalca          #+#    #+#             */
+/*   Updated: 2021/05/25 14:19:37 by vcavalca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (lst == NULL)
+		return (NULL);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
