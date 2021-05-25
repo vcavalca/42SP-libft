b5b22c81a98a8198e7e1ada1d522bc5d45b8e5a9
Vinicius <viniciusc.moura@icloud.com>
2021-05-25 11:20:02 -0300

    Made function substr

---
+++ ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/25 10:56:55 by vcavalca          #+#    #+#             */
+/*   Updated: 2021/05/25 11:04:23 by vcavalca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*new_s;
+	size_t	new_len;
+
+	if (s == NULL)
+		return (NULL);
+	if ((unsigned int)ft_strlen(s) < start)
+		return (ft_strdup(""));
+	new_len = ft_strlen(s + start);
+	if (new_len < len)
+		len = new_len;
+	new_s = (char *)ft_strmalloc(len);
+	if (new_s == NULL)
+		return (NULL);
+	ft_strlcpy(new_s, s + start, len + 1);
+	return (new_s);
+}
