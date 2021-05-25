86d3f3b1e80da3c2034ce2e7ab8e4c5e0d523d14
Vinicius <viniciusc.moura@icloud.com>
2021-05-25 16:55:20 -0300

    Update

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 09:36:19 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/05/25 09:42:22 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/05/25 16:55:00 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	new_haystack = (char *)haystack;
 	new_needle = (char *)needle;
 	if (new_needle[i] == '\0')
-		return (&new_haystack[i]);
+		return (new_haystack);
 	while (new_haystack[i] && i < len)
 	{
 		j = 0;
 		while (new_needle[j] && new_haystack[i + j] == new_needle[j] && i + j < len)
 			j++;
 		if (new_needle[j] == '\0')
-			return (&new_haystack);
+			return (new_haystack);
 		i++;
 	}
 	return (NULL);
