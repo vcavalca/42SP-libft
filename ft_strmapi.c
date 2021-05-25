4bb0d1c21ce9b8fd07a3ec315551e75286f41174
Vinicius <viniciusc.moura@icloud.com>
2021-05-25 13:00:23 -0300

    Made function strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/25 12:55:19 by vcavalca          #+#    #+#             */
+/*   Updated: 2021/05/25 12:59:38 by vcavalca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*new_s;
+	unsigned int	i;
+
+	i = 0;
+	new_s = (char *)ft_strmalloc(ft_strlen(s));
+	if (new_s == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		new_s[i] = f(i, s[i]);
+		i++;
+	}
+	new_s[i] = '\0';
+	return (new_s);
+}
