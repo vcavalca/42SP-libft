6bd3af5937815e34fcbec89e338e74b81df1165b
Vinicius <viniciusc.moura@icloud.com>
2021-05-25 12:14:56 -0300

    Update

--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 20:49:25 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/05/24 20:52:30 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/05/25 12:14:19 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	char	*new_b;
 
