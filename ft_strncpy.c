875c2e4f321d5fbd29e14c9cbbde773a8e3c00e9
Vinicius <viniciusc.moura@icloud.com>
2021-05-25 12:13:57 -0300

    Update

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,13 +6,13 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 20:01:02 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/05/24 20:04:06 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/05/25 12:12:35 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strncpy(char *dest, const char *src, size_t n)
+char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
 	size_t	i;
 
