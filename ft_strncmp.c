ba7c59e6cbd020ea99abd93cde7c76b87d3872b3
Vinicius <viniciusc.moura@icloud.com>
2021-05-26 10:50:34 -0300

    Update

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 20:19:13 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/05/24 20:21:48 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/05/26 10:50:17 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while ((s1[i] || s2[i]) && (i < n))
+	while ((s1[i] || s2[i]) && s1[i] == s2[i] && (i < n))
 	{
 		if (s1[i] < s2[i])
 		{
@@ -29,5 +29,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		}
 		i++;
 	}
-	return (0);
+	return ((unsigned char)s1 - (unsigned char)s2[i]);
 }
