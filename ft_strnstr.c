3c3d31dd021443d0734d1d2dd60bb9bc7777bd90
Vinicius <viniciusc.moura@icloud.com>
2021-05-26 09:57:50 -0300

    Update

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 09:36:19 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/05/25 17:23:51 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/05/26 09:57:14 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 			&& new_haystack[i + j] == new_needle[j] && i + j < len)
 			j++;
 		if (new_needle[j] == '\0')
-			return (new_haystack);
+			return (new_needle);
 		i++;
 	}
 	return (NULL);
