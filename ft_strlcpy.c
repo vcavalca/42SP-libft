9af143bd002b0b70665a361360c9301aa3ee53e7
Vinicius <viniciusc.moura@icloud.com>
2021-05-27 10:46:10 -0300

    Update

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 20:06:53 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/05/26 16:13:39 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/05/27 10:45:55 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	if (!dst || !src)
 		return (0);
 	src_len = ft_strlen(src);
-	if (dstsize == 0)
+	if (dstsize <= 0)
 		return (src_len);
 	while (src[i] && i < dstsize)
 	{
