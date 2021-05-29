e0347b4549645c6a69a23ee4c49a949c2979b450
vcavalca <61808793+viniciuscmoura@users.noreply.github.com>
2021-05-29 19:47:50 -0300

    Update

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 20:06:53 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/05/29 17:42:28 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/05/29 19:44:58 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 
-	if (!dst && !src)
-		return (0);
 	i = 0;
-	if (dstsize <= 0)
+	if (dstsize == 0)
 		return (ft_strlen(src));
-	while (src[i] && i < dstsize)
+	while (src[i] && i < dstsize - 1)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	if (dstsize < ft_strlen(src))
-		dst[dstsize - 1] = '\0';
-	else if (dstsize != 0)
-		dst[i] = '\0';
+	dst[i] = '\0';
 	return (ft_strlen(src));
-	}
+}
