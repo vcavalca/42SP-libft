930977e65e2c453e64f6f3acc4c14e357e84130d
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-09 14:08:07 -0300

    Protection fix

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 11:21:27 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/05/25 17:21:56 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/09 14:07:45 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 	int		j;
 
+	if (s1 == NULL)
+		return (s2);
+	if (s2 == NULL)
+		return (s1);
 	new_s = (char *)ft_strmalloc((ft_strlen(s1) + ft_strlen(s2)));
-	if (new_s == NULL)
+	if (new_s == NULL || (!s1 && !s2))
 		return (NULL);
 	i = 0;
 	while (s1[i] != '\0')
