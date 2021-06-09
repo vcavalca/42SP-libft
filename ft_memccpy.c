e23d2338e91c4d1c4466f72f53fad3d684dca344
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-09 14:01:55 -0300

    Protection fix

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 08:46:42 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/06/09 13:59:27 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/09 14:01:39 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 
 	i = 0;
 	new_src = (unsigned char *)src;
-	new_dst = dst;
+	new_dst = (unsigned char *)dst;
 	while (i < n)
 	{
 		new_dst[i] = new_src[i];
 		if (new_src[i] == c)
-			return ((void *)dst + i + 1);
+			return ((unsigned char *)dst + i + 1);
 		i++;
 	}
 	return (NULL);
