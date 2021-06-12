5cf02f1d8e30f869d18a756b1a8199cbbd4e4626
vcavalca <61808793+vcavalca@users.noreply.github.com>
2021-06-12 11:06:32 -0300

    Protection fix update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,37 +6,50 @@
 /*   By: vcavalca <vcavalca@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 11:21:27 by vcavalca          #+#    #+#             */
-/*   Updated: 2021/06/09 14:09:20 by vcavalca         ###   ########.fr       */
+/*   Updated: 2021/06/12 11:05:14 by vcavalca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_join(char const *s1, char const *s2)
 {
-	char	*new_s;
+	char	*c_join;
 	int		i;
 	int		j;
 
-	if (s1 == NULL)
-		return ((char *)s2);
-	if (s2 == NULL)
-		return ((char *)s1);
-	new_s = (char *)ft_strmalloc((ft_strlen(s1) + ft_strlen(s2)));
-	if (new_s == NULL || (!s1 && !s2))
-		return (NULL);
 	i = 0;
+	c_join = (char *)ft_strmalloc((strlen(s1) + strlen(s2)));
+	if (c_join == NULL)
+		return (NULL);
 	while (s1[i] != '\0')
 	{
-		new_s[i] = s1[i];
+		c_join[i] = s1[i];
 		i++;
 	}
 	j = 0;
 	while (s2[j] != '\0')
 	{
-		new_s[i + j] = s2[j];
+		c_join[i + j] = s2[j];
 		j++;
 	}
-	new_s[i + j] = '\0';
+	c_join[i + j] = '\0';
+	return (c_join);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_s;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		return ((char *)s2);
+	if (s2 == NULL)
+		return ((char *)s1);
+	new_s = (char *)ft_strmalloc((ft_strlen(s1) + ft_strlen(s2)));
+	if (new_s == NULL)
+		return (NULL);
+	new_s = ft_join(s1, s2);
 	return (new_s);
 }
